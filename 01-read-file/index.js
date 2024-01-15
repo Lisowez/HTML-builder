94bcccf66ed59d15edb1131ee2b332a2f45ec67c
Lisowez <lisowez@icloud.com>
2024-01-16 00:24:14 +0300

    fix: fix utf-8 in  task№1 'read-file'

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,7 +1,7 @@
 const fs = require('fs'); // подключаю модуль для управления файловой системой
 const path = require('path'); // подключаю модуль для работы с путями к файлам и каталогами
 const textPath = path.join(__dirname, 'text.txt'); // нахожу путь к файлу text.txt
-const read = fs.ReadStream(textPath, 'utf8'); //создание читабельного потока
+const read = fs.ReadStream(textPath, 'utf-8'); //создание читабельного потока
 read.on('data', (data) => {
   console.log(data); //чтение text.txt
 });
