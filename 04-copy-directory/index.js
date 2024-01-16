3ae56a22cc95cabc37044653d97cd1cebffb3f66
Lisowez <lisowez@icloud.com>
2024-01-16 21:30:24 +0300

    feat: done task№4 'copy-directory'

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -2,16 +2,16 @@ const fs = require('fs'); // подключаю модуль для управл
 const path = require('path'); // подключаю модуль для работы с путями к файлам и каталогами
 const pathClone = path.join(__dirname, 'files-copy'); // создание пути папки-клона
 const pathTrue = path.join(__dirname, 'files'); // создание пути папки-оригинала
-fs.rm(pathClone, { recursive: true, force: true }, (err) => { // удаление содержимого папки-клона
-  if (err) console.log(err);
-  fs.mkdir(pathClone, { recursive: true }, (err) => { //создание папки-клона
-    if (err) console.log(err);
-    fs.readdir(pathTrue, { withFileTypes: true }, (err, files) => { //чтение папки-оригинала и создание массива объектов, находящихся в ней
-      if (err) console.log(err);
+fs.rm(pathClone, { recursive: true, force: true }, (err) => {
+  if (err) console.log(err); // удаление содержимого папки-клона
+  fs.mkdir(pathClone, { recursive: true }, (err) => {
+    if (err) console.log(err); // ^ создание папки-клона
+    fs.readdir(pathTrue, { withFileTypes: true }, (err, files) => {
+      if (err) console.log(err); // ^ чтение папки-оригинала и создание массива объектов, находящихся в ней
       files.forEach((x) => {
         const pathFile = path.join(x.path, x.name); //создание пути каждого из объектов в папке-оригинале
-        fs.copyFile(pathFile, path.join(pathClone, x.name), (err) => { //клонирование каждого объекта из папки-оригинала в папку-клон
-          if (err) console.log(err);
+        fs.copyFile(pathFile, path.join(pathClone, x.name), (err) => {
+          if (err) console.log(err); // ^ клонирование каждого объекта из папки-оригинала в папку-клон
         });
       });
     });
